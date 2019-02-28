e8b151512e9a6824b87901ad690b79b66cc1bfaa
Payman Kossari <Paymankossari@gmail.com>
2019-02-28 14:52:40 -0500

    initial starting point, current branch

--- bookmark-client/src/App.test.js
+++ bookmark-client/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Nav from './Nav';
+import { BrowserRouter } from 'react-router-dom'
+import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Nav />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
